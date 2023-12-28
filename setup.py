9e94f0a3059c2bacd2092806a5d7411658f034f2
Ray Liu <ray@rayliu.ca>
2023-12-28 14:31:36 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='T-Rag',
     packages=find_packages(),
     version='0.1.0',
     description='Translation using retrieval augmented generation',
