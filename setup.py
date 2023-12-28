9c4f69809ed157158d19df9e47fcb260d37ed231
Ray Liu <ray@rayliu.ca>
2023-12-28 14:29:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='Translation using retrieval augmented generation',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Ray Liu',
     license='MIT',
 )
